f213ddc56156415d4a06dd1b61911c59972c7c9f
emhoo <emhoo@live.unc.edu>
2017-09-13 14:37:16 -0400

    added logging/finalized program

--- flurry/flurry.py
+++ flurry/flurry.py
@@ -2,44 +2,51 @@
 import requests
 import json
 import datetime
+import time
 import os
 import traceback
 # Import API secrets file
 import secrets 
 
-print("imported all libraries")
+# Create a file to log program progress as it runs
+log_file = open("//CHFS/Shared Documents/OpenData/datasets/logfiles/flurrylog.txt", "a")
+# Write date at beginning of log file
+now = str(datetime.date.today())
+log_file.write(now +"\n")
 
 # Access specific flurry query, pass header authentication, store data in var
 def get_flurry():
     
     # Find date and create vars to hold the span of one day 
+    # Write in format sierra understands
     now = str(datetime.date.today())
     one_day_ago = str(datetime.date.today() - datetime.timedelta(days=1))
     date_string = one_day_ago + "/" + now
     
     # Flurry API call, date string is updated to cover daily stats
     url = "https://api-metrics.flurry.com/public/v1/data/appUsage/day/company/app/language?metrics=sessions,activeDevices,newDevices,timeSpent,averageTimePerDevice,averageTimePerSession&dateTime=" + date_string
-    # Take API token from separate file
+    # Take API token from secrets file
     headers = {"Authorization":"Bearer " + str(secrets.flurry_api_token)}
+    # Access json data
     data = requests.get(url,headers=headers).json()
     data_list = data["rows"]
     
-    print("data fetched")
+    # Write success to log file 
+    log_file.write("Flurry data successfully accessed." + "\n")
     
     # Open the file to write/append to 
+    # Stored in flurry folder with open data directory
     flurrycsv = open('//CHFS/Shared Documents/OpenData/datasets/flurrydata/flurry.csv', "a")
-    print("flurry opened")
-    
+
     # Write CSV headings if the file is empty
     if os.stat("//CHFS/Shared Documents/OpenData/datasets/flurrydata/flurry.csv").st_size == 0:
         flurrycsv.write("Company Name, Average Time Per Session, Active Devices, Language, Time Spent, Sessions, Date and Time, Average Time Per Device, App Name, New Devices"+"\n")
-    print("added headings")
-    
+
     # Set counter
     i = 0
 
     # Write/append the data to the file once the headings have been added
-    # Write in CSV format for ease of use (since I'm not very familiar with json format)
+    # Write data in CSV format
     while i < len(data_list):
         flurrycsv.write(data_list[i]['company|name'] + ", ")
         flurrycsv.write(str(data_list[i]['averageTimePerSession'])+ ", ")
@@ -55,23 +62,25 @@ def get_flurry():
         i += 1 
     flurrycsv.close()
     
-    # Copy file to unpub direct
+    # Write success to log file 
+    log_file.write("Flurry data successfully written to flurrydata directory. \n")
+    
+    # Copy flurry file to unpub direct
     f = open('//CHFS/Shared Documents/OpenData/datasets/flurrydata/flurry.csv')
     f1 = open("//CHFS/Shared Documents/OpenData/datasets/unpublished/flurry.csv", "w")
     for line in f:
         f1.write(line)
-        
+    log_file.write("Flurry data successfully copied into unpublished directory." + "\n")
    
 # Main function
 def main():
     
-    errors = open("log_flurryerror.txt", "w")
+    # Handle exceptions, print errors to log file
     try:
         get_flurry()
     except Exception as exc:
-        now = str(datetime.date.today())
-        errors.write(traceback.format_exc())
-        errors.write(now)
+        log_file.write("There was an error running the program.")
+        log_file.write(traceback.format_exc() + "\n")
 
 # Call main
 main()
